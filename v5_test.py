a32378ac9dc4e024060d27758cf715a6a3cc8644
AbdulDag <Dagabdoo@gmail.com>
2026-03-09 19:16:39 -0400

    base

--- v5_test.py
+++ v5_test.py
@@ -1,8 +1,21 @@
 import cv2
 import numpy as np
-from ultralytics import YOLO
+from ultralytics import YOLO as yolo
 import sys
+import torch 
+
 s = 0
+#load weights from .pth file
+state_dict = torch.load('best_cow_classivier_v4')
+
+#yolo model with architecture i wanna use
+model = yolo('yolov8n.yaml')
+
+#load .pth weights
+model.model.load_state_dict(state_dict)
+
+#save it as a.pt file for future use
+model.save('converted_model.ptv4')
 
 #changes source to sys.arv[1]
 #e.g if python myscript.py cow_video.mp4 (cow_video.mp3 is sys.argv[1])
